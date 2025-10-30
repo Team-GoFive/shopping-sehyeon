18628184053d1444bc225701298de862859a2d36
terry <tryterry77@gmail.com>
2025-10-30 23:57:01 +0900

    feat: user 생성 시 유효성 검증 추가

--- src/main/java/com/kt/dto/UserCreateRequest.java
+++ src/main/java/com/kt/dto/UserCreateRequest.java
@@ -5,16 +5,29 @@ import java.time.LocalDate;
 import com.kt.domain.Gender;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
-// loginId, password, name, birthday(YYYY-mm-dd)
 public record UserCreateRequest(
 	@NotBlank
 	String loginId,
+	@NotBlank
+	// 최소 8자, 대문자, 소문자, 숫자, 특수문자 포함
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")
 	String password,
+	@NotBlank
 	String name,
+	@NotBlank
+	// 이메일 형식
+	@Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")
 	String email,
+	@NotBlank
+	// 000-0000-0000
+	@Pattern(regexp = "\\d{3}-\\d{4}-\\d{4}")
 	String mobile,
+	@NotNull
 	Gender gender,
+	@NotNull
 	LocalDate birthday
 ) {
 }
