ca9b450d4a1d361f063d61f26b138b8323b9e10e
terry <tryterry77@gmail.com>
2025-11-03 14:09:30 +0900

    feat: add user update endpoint and validation for update request

--- src/main/java/com/kt/dto/UserUpdateRequest.java
+++ src/main/java/com/kt/dto/UserUpdateRequest.java
@@ -2,10 +2,15 @@ package com.kt.dto;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.NotBlank;
+
 // 수정 가능 옵션: 비밀번호, 이름, 생일
 public record UserUpdateRequest(
-	String password,
+	@NotBlank
 	String name,
-	LocalDate birthday
+	@NotBlank
+	String mobile,
+	@NotBlank
+	String email
 ) {
 }
