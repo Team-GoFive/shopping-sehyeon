a3f163ace00182ccb750bbc5b08db8cbcad4d372
terry <tryterry77@gmail.com>
2025-11-03 10:00:55 +0900

    feat: 중복 로그인 체크 api 개발

--- src/main/java/com/kt/controller/UserController.java
+++ src/main/java/com/kt/controller/UserController.java
@@ -19,6 +19,8 @@ import com.kt.dto.UserCreateRequest;
 import com.kt.dto.UserUpdateRequest;
 import com.kt.service.UserService;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -32,33 +34,51 @@ public class UserController {
 	@ResponseStatus(HttpStatus.CREATED)
 	// json 형태의 body에 담겨서 post 요청으로 /users로 들어오면
 	// @RequestBody를 보고 jacksonObjectMapper가 동작해서 json을 읽어서 dto로 변환
-	public void create(@RequestBody UserCreateRequest request){
+	public void create(@RequestBody UserCreateRequest request) {
 		// jackson object mapper -> json to dto 매핑
 		userService.create(request);
 	}
 
+	// boolean, Boolean 차이
+	// boolean: primitive type, null 불가능, 기본값 false
+	// Boolean: reference type, null 가능
+	// null 가능성을 고려해야 할 때 Boolean 사용
+	@ApiResponses(
+		value = {
+			@ApiResponse(responseCode = "200", description = "중복 여부 반환"),
+			@ApiResponse(responseCode = "400", description = "잘못된 요청"),
+			@ApiResponse(responseCode = "500", description = "서버 오류")
+		}
+	)
+	@GetMapping("/duplicate-login-id")
+	@ResponseStatus(HttpStatus.OK)
+	public Boolean isDuplicateLoginId(@RequestParam String loginId) {
+		return userService.isDuplicateLoginId(loginId);
+	}
+
+
 	@GetMapping
 	@ResponseStatus(HttpStatus.OK)
-	public List<User> getUsers(){
+	public List<User> getUsers() {
 		return userService.getUsers();
 	}
 
 	// user 조회(loginId)
 	@GetMapping("/{loginId}")
 	@ResponseStatus(HttpStatus.OK)
-	public User getUser(@PathVariable String loginId){
+	public User getUser(@PathVariable String loginId) {
 		return userService.getUser(loginId);
 	}
 
 	@PutMapping("/{loginId}")
 	@ResponseStatus(HttpStatus.OK)
-	public void update(@PathVariable String loginId, @RequestBody UserUpdateRequest request){
+	public void update(@PathVariable String loginId, @RequestBody UserUpdateRequest request) {
 		userService.update(loginId, request);
 	}
 
 	@DeleteMapping("/{loginId}")
 	@ResponseStatus(HttpStatus.OK)
-	public void delete(@PathVariable String loginId){
+	public void delete(@PathVariable String loginId) {
 		userService.delete(loginId);
 	}
 }
