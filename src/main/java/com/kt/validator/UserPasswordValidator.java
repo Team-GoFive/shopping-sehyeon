90f40972d63454280ffb38f66dddc5416ab616d0
terry <tryterry77@gmail.com>
2025-11-07 23:38:12 +0900

    refactor: User 서비스 리팩토링, ErrorCode 추가

---
+++ src/main/java/com/kt/validator/UserPasswordValidator.java
@@ -0,0 +1,17 @@
+package com.kt.validator;
+
+import com.kt.common.CustomException;
+import com.kt.common.ErrorCode;
+import com.kt.domain.User;
+
+public class UserPasswordValidator {
+
+	public static void validate(String oldPassword, String password, User user) {
+		if (oldPassword.equals(password)) {
+			throw new CustomException(ErrorCode.SAME_OLD_PASSWORD);
+		}
+		if (!user.getPassword().equals(oldPassword)) {
+			throw new CustomException(ErrorCode.INVALID_OLD_PASSWORD);
+		}
+	}
+}
