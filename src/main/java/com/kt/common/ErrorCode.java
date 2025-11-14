654570fce60d7dea5f63e3faf950f0878bc5eeda
terry <tryterry77@gmail.com>
2025-11-15 00:59:14 +0900

    feat: redis를 통한 분산락 구현

--- src/main/java/com/kt/common/ErrorCode.java
+++ src/main/java/com/kt/common/ErrorCode.java
@@ -14,7 +14,9 @@ public enum ErrorCode {
 	SAME_OLD_PASSWORD(HttpStatus.BAD_REQUEST, "기존 비밀번호와 동일한 비밀번호로 변경할 수 없습니다."),
 	INVALID_OLD_PASSWORD(HttpStatus.BAD_REQUEST, "기존 비밀번호가 일치하지 않습니다."),
 
-	INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "유효하지 않은 요청 파라미터입니다");
+	INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "유효하지 않은 요청 파라미터입니다"),
+	FAIL_ACQUIRED_LOCK(HttpStatus.BAD_REQUEST, "락 획득에 실패했습니다."),
+	ERROR_SYSTEM(HttpStatus.INTERNAL_SERVER_ERROR, "시스템 오류가 발생했습니다.");
 
 	private final HttpStatus status;
 	private final String message;
