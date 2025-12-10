d4ac981978a1176e80c2f6b0680ea572e87c8cb6
terry <tryterry77@gmail.com>
2025-12-11 00:38:41 +0900

    fix: 테스트 주석

--- src/main/java/com/kt/config/ActiveProfileConfig.java
+++ src/main/java/com/kt/config/ActiveProfileConfig.java
@@ -1,10 +1,6 @@
 package com.kt.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-
-import com.kt.common.profile.TestProfile;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,20 +8,20 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ActiveProfileConfig {
 
-	private final Environment environment;
-
-	@TestProfile
-	private final TestProperties testProperties;
-
-	private final CommonProperties commonProperties;
-
-	@Bean
-	public String init() {
-		System.out.println(commonProperties.commonValue());
-		System.out.println(commonProperties.commonValueParam());
-		System.out.println(testProperties.myValue());
-		System.out.println(testProperties.myValueParam());
-		// System.out.println("Active Profile : " + environment.getActiveProfiles()[0]);
-		return "";
-	}
+	// private final Environment environment;
+	//
+	// @TestProfile
+	// private final TestProperties testProperties;
+	//
+	// private final CommonProperties commonProperties;
+	//
+	// @Bean
+	// public String init() {
+	// 	System.out.println(commonProperties.commonValue());
+	// 	System.out.println(commonProperties.commonValueParam());
+	// 	System.out.println(testProperties.myValue());
+	// 	System.out.println(testProperties.myValueParam());
+	// 	// System.out.println("Active Profile : " + environment.getActiveProfiles()[0]);
+	// 	return "";
+	// }
 }
